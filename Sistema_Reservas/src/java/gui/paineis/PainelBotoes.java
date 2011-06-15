0a80f4655a7092229d9e977f239a1ff8b49ae94a
vinicius.ov <vinicius.ov@5db61ee6-28fc-22f7-3e44-a231e9659209>
2011-06-15 12:57:51 +0000

    classes dao

---
+++ Sistema_Reservas/src/java/gui/paineis/PainelBotoes.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui.paineis;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Window;
+import gui.janelas.JanelaGerenciarSalas;
+import gui.janelas.JanelaPesquisas;
+
+/**
+ *
+ * @author 0213101
+ */
+public class PainelBotoes extends Panel{
+
+    Label lInicio;
+    Button botaoLogin;
+    Button botaoSair;
+    Button botaoNovaReserva;
+    Button botaoConsultaSala;
+    Button botaoPesquisar;
+    HorizontalLayout leiaute;
+    Window win;
+    FormLogin pl;
+
+    public PainelBotoes(Window win)
+    {
+        pl = new FormLogin();
+        this.win = win;
+        lInicio = new Label("<b>Sistema de Reserva de Salas 2011</b>",Label.CONTENT_XHTML);
+        botaoLogin = new Button();
+        botaoLogin.setCaption("Entrar no sistema");
+        botaoLogin.addListener(new EventoLogin());
+        botaoSair = new Button();
+        botaoSair.setCaption("Encerrar");
+        
+        botaoPesquisar = new Button("Pesquisar...");
+        botaoPesquisar.addListener(new EventoPesquisar());
+        /*
+        botaoNovaReserva.setCaption("Nova Reserva");
+        botaoNovaReserva.addListener(new EventoNovaReserva());
+        *
+        */
+        botaoConsultaSala = new Button();
+        botaoConsultaSala.setCaption("Gerenciar Salas ...");
+        botaoConsultaSala.addListener(new EventoNovaSala());
+        
+        leiaute = new HorizontalLayout();
+        leiaute.addComponent(pl);
+        pl.setVisible(false);
+        leiaute.addComponent(botaoLogin);
+        leiaute.addComponent(botaoSair);
+        leiaute.addComponent(botaoConsultaSala);
+        leiaute.addComponent(botaoPesquisar);
+        this.addComponent(lInicio);
+        this.addComponent(leiaute);
+
+        
+    }
+
+    
+    //maneira de fazer eventos para os botoes
+    private class EventoNovaSala implements Button.ClickListener
+    {
+    @Override
+        public void buttonClick(Button.ClickEvent event)
+        {
+            JanelaGerenciarSalas FNS = new JanelaGerenciarSalas();
+            win.addWindow(FNS);
+
+        }
+
+    }
+
+    private class EventoPesquisar implements Button.ClickListener
+    {
+    @Override
+        public void buttonClick(Button.ClickEvent event)
+        {
+            JanelaPesquisas jp = new JanelaPesquisas();
+            win.addWindow(jp);
+
+        }
+
+    }
+    
+    private class EventoLogin implements Button.ClickListener
+    {
+    @Override
+        public void buttonClick(Button.ClickEvent event)
+        {
+            if (pl.isVisible())
+                pl.setVisible(false);
+            else
+                pl.setVisible(true);
+        }
+    }
+
+}
+
